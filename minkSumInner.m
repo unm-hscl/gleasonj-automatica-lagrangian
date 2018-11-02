49431dc2e658c31f9137ac80727bcdcbb50e33c9
Abraham <aby.vinod@gmail.com>
2018-11-01 22:09:51 -0600

    minkSumInner tested for multiple cases
    - Template polytope needs to be centered for maximum benefit of scaling
    - Check for dimension mismatch as well as empty polytopes
    - Set bisection to 1e-2
    - Opt value of min-max due to use of fmincon is declared to be zero if
    it is above myzero = -1e-6
    - Instead of fminunc, we use fmincon with l drawn from a l_\infty box
    - Optval of cvx is retained if it is solved, else if it is unbounded set
    to a large positive value, and else if it is infeasible set to a large
    negative value so that fmincon will not freak out.

--- minkSumInner.m
+++ minkSumInner.m
@@ -10,6 +10,14 @@ function polyInner = minkSumInner(polyA, polyB, varargin)
     else
         polyC = polyA;
     end
+    if isEmptySet(polyA) || isEmptySet(polyB) || isEmptySet(polyC)
+        throwAsCaller(SrtInvalidArgsError('Empty set provided as arguments'));
+    end
+    if ~((polyA.Dim == polyB.Dim) && (polyB.Dim == polyC.Dim))
+        throwAsCaller(SrtInvalidArgsError('Mismatch in polytope dimensions'));
+    end
+    % Ensure that polyC is "centered" (This way uniform scaling is not partial)
+    polyC = polyC - polyC.chebyCenter.x;    
     
     % To simplify the problem, we will not optimize for the shift but
     % simply use the minkowski sum of Chebyshev centers (deepest points)
@@ -17,22 +25,26 @@ function polyInner = minkSumInner(polyA, polyB, varargin)
     % point exactly?
     xc_guess = polyA.chebyCenter().x + polyB.chebyCenter().x;
     
+    
     % Bisect
     t_ub = 10;
     t_lb = 0;
-    bisect_tol = 1e-3;
+    bisect_tol = 1e-2;
+    myzero_lb = -1e-6;
     if outer_opt(t_ub, xc_guess, polyA, polyB, polyC) >= 0
         warning('SReachTools:runtime',['10x magnification of the template',...
             ' is a subset of the Minkowski sum. Please use a larger ',...
             'polytope for better results.']);
         t_opt = t_ub;
     else
+        fprintf('Bisection upper bound of t=%1.3f is valid\n',t_ub);
+        t_opt = 0;
         while abs(t_ub - t_lb) > bisect_tol
             t_try = (t_ub + t_lb)/2;
             optval = outer_opt(t_try, xc_guess, polyA, polyB, polyC);
-            if optval >= 0
-                t_lb = t_try;
+            if optval >= myzero_lb
                 t_opt = t_try;
+                t_lb = t_try;
                 fprintf('t=%1.3f:   Feasible\n',t_try);
             else
                 t_ub = t_try;
@@ -40,6 +52,11 @@ function polyInner = minkSumInner(polyA, polyB, varargin)
             end
         end
     end    
+    if t_opt < bisect_tol
+        warning('SReachTools:runtime', sprintf(['%1.2fx magnification of ',...
+            'the template is also not a subset of the Minkowski sum. Please',...
+            ' use a smaller polytope for a non-trivial results.'],bisect_tol));
+    end
     xc_opt = xc_guess;
     polyInner = t_opt * polyC + xc_opt;
 end
@@ -48,8 +65,10 @@ function optval = outer_opt(t, xc, polyA, polyB, polyC)
     % Compute the support function vector that can violate containment,
     % i.e. inf_l sup_{lambda, y, z} containment_obj
     % We need this value to be positive for guranteeing containment
-    options = optimoptions('fminunc','Display','off','OptimalityTolerance',1e-8);
-    [~,optval] = fminunc(@(ell) inner_opt(ell, t, xc, polyA, polyB, polyC), rand(polyA.Dim,1),options);
+    options = optimoptions('fmincon','Display','off');
+    [~,optval] = fmincon(@(ell) inner_opt(ell, t, xc, polyA, polyB, polyC),...
+        0.5*ones(polyA.Dim,1), [], [], [], [],...
+        -ones(polyA.Dim,1), ones(polyA.Dim,1), [], options);
 end
 
 function optval = inner_opt(ell, t, xc, polyA, polyB, polyC)
@@ -57,7 +76,9 @@ function optval = inner_opt(ell, t, xc, polyA, polyB, polyC)
     % given support function vector ell, scaling t, shift xc and polytopes
     % polyA, polyB (Minkowski summands) and polyC (template polytope that
     % is to be scaled and shifted)    
+    cvx_clear
     cvx_begin quiet
+        cvx_precision best
         variable lambda(size(polyC.H,1),1);
         variable y(polyA.Dim,1);
         variable z(polyB.Dim,1);
@@ -67,9 +88,19 @@ function optval = inner_opt(ell, t, xc, polyA, polyB, polyC)
             polyA.A * y <= polyA.b;
             polyB.A * z <= polyB.b;
             lambda >= 0;
-            polyC.A' * lambda == t * ell;
-    cvx_end
-    optval = cvx_optval;
+            polyC.A' * lambda == t * ell;            
+    cvx_end    
+    optval = cvx_optval;  
+    if strcmpi(cvx_status,'Solved')        
+    elseif strcmpi(cvx_status,'Unbounded')
+        % Setting it to a large pos value so that fmincon won't freak out
+        optval = 1e10;
+    elseif strcmpi(cvx_status,'Infeasible')
+        % Setting it to a large neg value so that fmincon won't freak out
+        optval = -1e10;
+    else
+        keyboard
+    end
 end
 
 
