5b4db58057d0ec7eb16d3c65bd7b0a4112bf2f34
Joseph Gleason <gleasonj@unm.edu>
2019-11-26 12:08:18 -0700

    added Figure.m files

---
+++ Figure2.m
@@ -0,0 +1,201 @@
+% 
+% Filename    : Figure3.m
+% Author      : Joseph Gleason
+% Description : Script to generate Figure 3 of Automatica paper "Lagrangian
+%               Approximations for Stochastic Reachability of a Target Tube."
+%
+%	        Dubins Car Fiure
+%
+
+% Prescript running: Initializing srtinit, if it already hasn't been initialized
+clearvars;
+close all;
+
+% Time horizon
+time_horizon = 21;
+init_heading = pi/10;
+sampling_time = 0.1;
+box_halflength = 4;
+omega = pi/time_horizon/sampling_time;
+turning_rate = omega*ones(time_horizon,1);
+umax = 10;
+input_space = Polyhedron('lb',0,'ub',umax);
+n_mcarlo_sims = 1e3;                        % Monte-Carlo simulation particles
+n_mcarlo_sims_affine = 1e3;                 % For affine controllers
+
+% LTV system definition
+% Disturbance matrix and random vector definition
+dist_matrix = eye(2);
+eta_dist_nongauss = RandomVector('UserDefined', @(N) ...
+    0.1*(betarnd(2, 2, 2, N) - 0.5));
+
+[sys, heading_vec] = getDubinsCarLtv('add-dist', turning_rate, ...
+    init_heading, sampling_time, input_space, dist_matrix, eta_dist_nongauss);
+
+% Compute the mean trajectory of the concatenated disturbance vector
+muW_nongauss = sys.dist.concat(time_horizon).mean();
+mu = reshape(muW_nongauss, 2, []);
+
+
+
+% Probability threshold of interest
+prob_thresh = 0.8;
+
+v_nominal = umax * 2/3;                 % Nominal trajectory's heading velocity
+                                        % TODO: Gaussian was 3/2
+% Construct the nominal trajectory
+[~,H,~] = sys.getConcatMats(time_horizon);
+center_box_X = [zeros(2,1);H * (v_nominal * ones(time_horizon,1))];
+center_box = reshape(center_box_X,2,[]);
+% Box sizes
+box_halflength_at_0 = 4;                % Box half-length at t=0
+time_const = 1/2*time_horizon;          % Time constant characterize the
+                                        % exponentially decaying box half-length
+
+% Target tube definition as well as plotting
+target_tube_cell = cell(time_horizon + 1,1);
+for itt = 0:time_horizon
+    % Define the target set at time itt
+    target_tube_cell{itt+1} = Polyhedron(...
+        'lb',center_box(:, itt+1) -box_halflength_at_0*exp(- itt/time_const),...
+        'ub', center_box(:, itt+1) + box_halflength_at_0*exp(- itt/time_const));
+end
+
+target_tube = Tube(target_tube_cell{:});
+
+% Underapproximation of the stochastic reach set
+n_dim = sys.state_dim + sys.input_dim;
+timer_lagunder_options = tic;
+theta_polytope_vec = linspace(0,2*pi,10)';
+template_poly = Polyhedron('V',[cos(theta_polytope_vec),sin(theta_polytope_vec)]);
+template_poly.computeHRep();
+lagunder_options = SReachSetOptions('term', 'lag-under', ...
+    'bound_set_method', 'polytope', 'template_polytope', ...
+    template_poly, ...
+    'compute_style', 'vfmethod', 'vf_enum_method', 'lrs', 'verbose', 2);
+    
+
+elapsed_time_lagunder_options = toc(timer_lagunder_options);
+
+timer_lagunder = tic;
+[polytope_lagunder, extra_info_under] = SReachSet('term', 'lag-under', ...
+    sys, prob_thresh, target_tube, lagunder_options);
+elapsed_time_lagunder = toc(timer_lagunder);
+
+% Overapproximation of the stochastic reach set
+n_dim = sys.state_dim;
+timer_lagover_options = tic;
+lagover_options = SReachSetOptions('term', 'lag-over', ...
+    'bound_set_method', 'polytope', 'template_polytope', ...
+    template_poly, ...
+    'compute_style', 'vfmethod', 'vf_enum_method', 'lrs', 'verbose', 2);
+elapsed_time_lagover_options = toc(timer_lagover_options);
+    
+timer_lagover = tic;
+polytope_lagover = SReachSet('term', 'lag-over', sys,  prob_thresh, ...
+    target_tube, lagover_options);
+elapsed_time_lagover = toc(timer_lagover);
+
+% Compute a far-away safe initial
+cvx_begin quiet
+    variable initial_state(sys.state_dim, 1)
+    minimize ([1 1]*initial_state)
+    subject to
+        polytope_lagunder.A*initial_state <= polytope_lagunder.b;
+        target_tube(1).A*initial_state <= target_tube(1).b;
+cvx_end
+
+srp_timer = tic;
+point_opts = SReachPointOptions('term', 'particle-open', ...
+    'n_particles', 100, 'verbose', 1, 'max_particles', 200);
+
+[p, u, K] = SReachPoint('term', 'particle-open', sys, initial_state, ...
+    target_tube, point_opts);
+srp_time = toc(srp_timer);
+
+% Create a controller based on the underapproximation
+srlcontrol = SReachLagController(sys, ... 
+    extra_info_under.bounded_dist_set, ...
+    extra_info_under.stoch_reach_tube);
+
+mX_particle   = zeros(2, time_horizon+1);
+mX_lagrangian = zeros(2, time_horizon+1);
+mX_particle(:, 1) = initial_state;
+mX_lagrangian(:, 1) = initial_state;
+for lv = 1:length(u)
+    mX_particle(:, lv+1) = sys.state_mat(lv-1) * mX_particle(:, lv) + ...
+        sys.input_mat(lv-1) * u(lv) + sys.dist_mat(lv-1) * mu(:, lv);
+    mX_lagrangian(:, lv+1) = sys.state_mat(lv-1) * mX_lagrangian(:, lv) + ...
+        sys.input_mat(lv-1) * srlcontrol.getInput(mX_lagrangian(:, lv), lv-1) + ...
+        sys.dist_mat(lv-1) * mu(:, lv);
+end
+
+%% Reporting solution times
+fprintf('Elapsed time (Lagrangian Underapproximation)      : %1.3f s\n', ...
+    elapsed_time_lagunder);
+fprintf('Elapsed time (Lagrangian Overapproximation)       : %1.3f s\n', ...
+    elapsed_time_lagover);
+% fprintf('Elapsed time (Offline): (lag-under) %1.3f | (lag-over) %1.3f s\n', ...
+%     elapsed_time_lagunder_options, elapsed_time_lagover_options);
+fprintf('Elapsed time (SReachPoint Particle Approximation) : %1.3f s\n', ...
+    srp_time);
+
+% Plotting
+hf = figure();
+hold on;
+for lt = length(target_tube):-1:1
+    plot(target_tube(lt), 'Color', 'y');
+end
+
+for lv = 2:length(hf.Children.Children)
+    ptch = hf.Children.Children(lv);
+    ptch.FaceAlpha = 0.2;
+    ptch.EdgeAlpha = 0.2;
+end
+
+plot(polytope_lagover, 'color', 'r');
+plot(polytope_lagunder, 'color', 'g');
+
+xlabel('$x$','interpreter','latex');
+ylabel('$y$','interpreter','latex');
+
+plot(mX_particle(1, :), mX_particle(2, :), '-o', ...
+    'Color', 'm', ...
+    'DisplayName', 'Particle Approximation Mean Trajectory');
+plot(mX_lagrangian(1, :), mX_lagrangian(2, :), '-^', ...
+    'Color', 'k', ...
+    'DisplayName', 'Lagrangian Mean Trajectory');
+
+hf.Units = 'inches';
+hf.Position = [hf.Position(1:2), 3.1979, 4.7917];
+
+box on;
+
+ha = gca;
+axis equal;
+ha.XLim = [-4.2000, 5.5000];
+ha.YLim = [-4.2000, 10];
+ha.Position = [0.1628, 0.2496, 0.7158, 0.7287];
+ha.TickLabelInterpreter = 'latex';
+
+ha.Children(1).MarkerFaceColor = ha.Children(1).Color;
+ha.Children(2).MarkerFaceColor = ha.Children(2).Color;
+
+ha.Children(3).DisplayName = 'Lagrangian Underapproximation';
+ha.Children(4).DisplayName = 'Lagrangian Overapproximation';
+ha.Children(5).DisplayName = 'Target Tube';
+
+hl = legend(ha.Children(1:5));
+hl.Interpreter = 'latex';
+hl.Box = 'off';
+hl.Position = [0.0977, 0.0531, 0.7983, 0.1247];
+
+
+print(hf, '-r200', '-dpng', sprintf('var/figs/dubincs-car-%s.png', ...
+    datestr(now, 'yyyy-mm-dd-HHMMSS')));
+
+% fprintf('Monte-Carlo simulation: Success prob: %1.3f | Time: %1.3f s\n', ...
+%     size(X,2)/n_mcarlo_sims, mc_time);
+
+% savefig(hf, sprintf('~/Dropbox/dubins-automatica-%s.fig', datestr(now, 'yyyy-mm-dd-HHMMSS')));
+% save(sprintf('~/Dropbox/dubins-automatica-%s.mat', datestr(now, 'yyyy-mm-dd-HHMMSS')));
---
+++ Figure3.m
@@ -0,0 +1,197 @@
+%
+% Name        : Figure3.m
+% Authors     : Joseph D. Gleason and Abraham P. Vinod
+% Date        : 2018-10-12
+%
+% Description : Generate Figure 3 from submitted work; verification of satellite
+%               rendezvous-docking problem using Clohessy-Wiltshire-Hill
+%               dynamics
+% 
+
+close all;
+clearvars;
+
+%% System definition
+umax = 0.1;
+mean_disturbance = zeros(4,1);
+covariance_disturbance = diag([1e-4, 1e-4, 5e-8, 5e-8]);
+% Define the CWH (planar) dynamics of the deputy spacecraft relative to the
+% chief spacecraft as a LtiSystem object
+sys = getCwhLtiSystem(4, Polyhedron('lb', -umax*ones(2,1),...
+                                    'ub',  umax*ones(2,1)),...
+       RandomVector('Gaussian', mean_disturbance,covariance_disturbance));
+
+%% Target tube construction --- reach-avoid specification
+time_horizon=5;          % Stay within a line of sight cone for 4 time steps and 
+                         % reach the target at t=5% Safe Set --- LoS cone
+% Safe set definition --- LoS cone |x|<=y and y\in[0,ymax] and |vx|<=vxmax and 
+% |vy|<=vymax
+ymax = 2;
+vxmax = 0.5;
+vymax = 0.5;
+A_safe_set = [1, 1, 0, 0;           
+             -1, 1, 0, 0; 
+              0, -1, 0, 0;
+              0, 0, 1,0;
+              0, 0,-1,0;
+              0, 0, 0,1;
+              0, 0, 0,-1];
+b_safe_set = [0;
+              0;
+              ymax;
+              vxmax;
+              vxmax;
+              vymax;
+              vymax];
+safe_set = Polyhedron(A_safe_set, b_safe_set);
+
+% Target set --- Box [-0.1,0.1]x[-0.1,0]x[-0.01,0.01]x[-0.01,0.01]
+target_set = Polyhedron('lb', [-0.1; -0.1; -0.01; -0.01],...
+                        'ub', [0.1; 0; 0.01; 0.01]);
+target_tube = Tube('reach-avoid',safe_set, target_set, time_horizon);                    
+slice_at_vx_vy = zeros(2,1);
+
+%% Preparation for set computation
+prob_thresh = 0.8;
+
+n_dir_vecs = 16;
+theta_vec = linspace(0, 2*pi, n_dir_vecs);
+set_of_dir_vecs_ft = [cos(theta_vec);
+                      sin(theta_vec);
+                      zeros(2,n_dir_vecs)];
+n_dir_vecs = 40;
+theta_vec = linspace(0, 2*pi, n_dir_vecs);
+set_of_dir_vecs_cc_open = [cos(theta_vec);
+                           sin(theta_vec);
+                           zeros(2,n_dir_vecs)];
+init_safe_set_affine = Polyhedron('He',[zeros(2,2) eye(2,2) slice_at_vx_vy]);
+
+%% CC (Linear program approach)
+options = SReachSetOptions('term', 'chance-open',...
+    'set_of_dir_vecs', set_of_dir_vecs_cc_open,...
+    'init_safe_set_affine', init_safe_set_affine, 'verbose', 1);
+timer_cc_open = tic;
+[polytope_cc_open, extra_info] = SReachSet('term','chance-open', sys,...
+    prob_thresh, target_tube, options);  
+elapsed_time_cc_open = toc(timer_cc_open);
+
+%% Fourier transform (Genz's algorithm and MATLAB's patternsearch)
+options = SReachSetOptions('term', 'genzps-open',...
+    'desired_accuracy', 5e-2, 'set_of_dir_vecs', set_of_dir_vecs_ft,...
+    'init_safe_set_affine', init_safe_set_affine, 'verbose', 1);
+timer_ft = tic;
+[polytope_ft, ~] = SReachSet('term','genzps-open', sys, prob_thresh,...
+    target_tube, options);  
+elapsed_time_ft = toc(timer_ft);
+
+%% Lagrangian underapproximation
+vecs_per_orth = [6, 10, 20, 50];
+lag_comptimes = zeros(size(vecs_per_orth));
+lag_polys = [];
+n_dim = sys.state_dim + sys.input_dim;
+for lv = 1:length(vecs_per_orth)
+    fprintf('Lagrangian approximation with %d vectors per orthant\n', ...
+        vecs_per_orth(lv));
+    fprintf('------------------------------------------------------------\n');
+    timer_lagunder_options = tic;
+    lagunder_options = SReachSetOptions('term', 'lag-under',...
+        'bound_set_method', 'ellipsoid', 'compute_style','support',...
+        'system', sys, 'vf_enum_method', 'lrs', 'verbose', 1,...
+        'n_vertices', 2^n_dim * vecs_per_orth(lv) + 2*n_dim);
+    elapsed_time_lagunder_options = toc(timer_lagunder_options);
+
+    disp(elapsed_time_lagunder_options)
+
+    timer_lagunder = tic;
+    [polytope_lagunder, extra_info_under] = SReachSet('term', 'lag-under', ...
+        sys, prob_thresh, target_tube, lagunder_options);
+    lag_comptimes(lv) = toc(timer_lagunder);
+    lag_polys = [lag_polys; polytope_lagunder];
+end
+
+%% Plotting and Monte-Carlo simulation-based validation
+hf = figure();
+box on;
+hold on;
+plot(safe_set.slice([3,4], slice_at_vx_vy), 'color', [0.95, 0.95, 0]);
+ha = gca;
+ha.Children(1).DisplayName = 'Safe Set';
+plot(target_set.slice([3,4], slice_at_vx_vy), 'color', [0, 0, 0]);
+ha.Children(1).DisplayName = 'Target Set';
+plot(polytope_cc_open.slice([3,4], slice_at_vx_vy), 'color',[1, 0.6, 0],'alpha', 1);
+ha.Children(1).DisplayName = 'Chance Constraint';
+plot(polytope_ft.slice([3,4], slice_at_vx_vy), 'color',[0, 0.6, 1],'alpha',1);
+ha.Children(1).DisplayName = 'Fourier Transforms';
+cl = [0, 0.5, 0.5];
+for lv = length(lag_polys):-1:1
+    poly = lag_polys(lv);
+    cl = cl + [0, 0.5/4, -0.5/4];
+    plot(poly.slice([3,4], slice_at_vx_vy), 'color', cl, 'alpha',1);
+    ha.Children(1).DisplayName = sprintf('Lagrangian Approximation: %d Directions', ...
+         2^n_dim * vecs_per_orth(lv) + 2*n_dim);
+end
+hl = legend();
+hl.Location = 'South';
+xlabel('$x$','interpreter','latex');
+ylabel('$y$','interpreter','latex');
+box on;
+grid on;
+
+% Formatting for paper
+ha.FontSize = 9;
+ha.TickLabelInterpreter = 'latex';
+hl.FontSize = 9;
+hl.Interpreter = 'latex';
+
+hf.Units = 'inches';
+hf.Position(3) = 3.2;
+
+ha.Position = [0.1497, 0.4677, 0.7650, 0.4892];
+hl.Poisiton = [0.0398, 0.0202, 0.9213, 0.3298];
+hl.Box = 'off';
+
+print(gcf, '-r200', '-dpng', sprintf('~/Dropbox/cwh-ex-%s.png', datestr(now, 'yyyy-mm-dd-HHMMSS')));
+
+%% Compute time
+fprintf('Elapsed time: \n');
+fprintf('    Fourier Transforms (genzps-open) %1.3f\n', elapsed_time_ft);
+fprintf('    Chance Constraints (chance-open) %1.3f\n', elapsed_time_cc_open);
+for lv = 1:length(vecs_per_orth)
+    fprintf('    Lagrangian Approximation with %d Directions %1.3f\n', ... 
+        2^n_dim * vecs_per_orth(lv) + 2*n_dim, lag_comptimes(lv)); 
+end
+
+%% Helper functions
+% Plotting function
+function [legend_cell] = plotMonteCarlo(method_str, mcarlo_result,...
+    concat_state_realization, n_mcarlo_sims, n_sims_to_plot, state_dim,...
+    initial_state, legend_cell)
+% Plots a selection of Monte-Carlo simulations on top of the plot
+
+    green_legend_updated = 0;
+    red_legend_updated = 0;
+    traj_indices = floor(n_mcarlo_sims*rand(1,n_sims_to_plot));
+    for realization_index = traj_indices
+        % Check if the trajectory satisfies the reach-avoid objective
+        if mcarlo_result(realization_index)
+            % Assign green triangle as the marker
+            plotOptions = {'Color', 'g', 'Marker', '^', ...
+                'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'g' 'MarkerSize', 5};
+            markerString = 'g^';
+        else
+            % Assign red asterisk as the marker
+            plotOptions = {'Color', 'r', 'Marker', 'x', ...
+                'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r' 'MarkerSize', 5};
+            markerString = 'rx';
+        end
+
+        % Create [x(t_1) x(t_2)... x(t_N)]
+        reshaped_X_vector = reshape(...
+            concat_state_realization(:,realization_index), state_dim,[]);
+
+        % This realization is to be plotted
+        h = plot([initial_state(1), reshaped_X_vector(1,:)], ...
+                 [initial_state(2), reshaped_X_vector(2,:)], ...
+                 plotOptions{:});
+    end
+end
